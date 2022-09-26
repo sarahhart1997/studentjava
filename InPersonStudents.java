d6a59e8cbbc8d47ddf29c208ec6b257ee5a4c420
Sarah Hart <sarah.hart.nc@gmail.com>
2022-09-26 19:47:50 -0400

    add constructors to child classes

--- InPersonStudents.java
+++ InPersonStudents.java
@@ -1,6 +1,7 @@
 public class InPersonStudents extends InPersonStudents
 {
     //Constructor from the parent class
-
+    super();
+    
     // Score abstract method from parent class
 }
--- RemoteStudents.java
+++ RemoteStudents.java
@@ -1,7 +1,15 @@
 public class RemoteStudents extends Students 
 {
+    // New attribute discussion 
+    String discussion;
+
     //Constructor from parent class w/ additional parameter discussion
+    super(discussion){
+        // connecting constructor to above?
+        this.discussion = discussion;
+        System.out.printIn("student constructor");
+    }
 
     // Abstract method score from parent class
-    
+
 }
