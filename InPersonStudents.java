42aeb8c4ab1bdc943323b389a53ebdf02b631199
Sarah Hart <sarah.hart.nc@gmail.com>
2022-09-26 23:32:10 -0400

    updates

--- InPersonStudents.java
+++ InPersonStudents.java
@@ -3,8 +3,6 @@ public class InPersonStudents extends InPersonStudents
     //Constructor from the parent class
     InPersonStudents(){
         super("Students");
-
-        System.out.printIn("In Person Constructor");
     }
 
     // Score abstract method from parent class add assignment percent
