2dbf12aedabb4dc447f5123b304602bafc71a39e
Sarah Hart <sarah.hart.nc@gmail.com>
2022-09-26 20:46:32 -0400

    update student scores

--- JavaProgramming.java
+++ JavaProgramming.java
@@ -4,11 +4,11 @@ public static void main (String[] args)
 
     // Custom attributes being put into the constructor
     Students a = new Students(
-        3245, "Jane", "Remote"
+        3245, "Jane", "Remote", 92, 83, 95.5, 95.7
     );
 
     Students b = new Students(
-        2872, "John", "In Person"
+        2872, "John", "In Person", 82.5, 80, 90.7
     );
 
     System.out.printIn(a);
