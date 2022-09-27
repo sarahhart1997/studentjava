844e4faa272bd81082532d454ddf02c01aeb70a8
Sarah Hart <sarah.hart.nc@gmail.com>
2022-09-26 23:27:18 -0400

    updates

--- JavaProgramming.java
+++ JavaProgramming.java
@@ -7,11 +7,13 @@ public static void main (String[] args)
         System.out.printIn("id:"+a.id);
         System.out.printIn("name:"+a.name);
         System.out.printIn("type:"+a.type);
-        //92, 83, 95.5, 95.7??? Somehow
+        //92, 83, 95.5, 95.7??? Somehow best guess
+        RemoteStudents c = new RemoteStudents(92, 83, 95.5, 95.7);
 
     Students b = new Students(2872, "John", "InPerson");
         System.out.printIn("id:"+a.id);
         System.out.printIn("name:"+a.name);
         System.out.printIn("type:"+a.type);
-        // Somehow add grades 82.5, 80, 90.7
+        // Somehow add grades 82.5, 80, 90.7 best guess
+        InPersonStudents d = new RemoteStudents(82.5, 80, 90.7);
 }
