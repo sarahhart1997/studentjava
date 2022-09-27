c7ffa381894cc634fc2a73acc28371a2c28ca45c
Sarah Hart <sarah.hart.nc@gmail.com>
2022-09-26 23:09:06 -0400

    add print functions

--- JavaProgramming.java
+++ JavaProgramming.java
@@ -3,13 +3,18 @@ public static void main (String[] args)
     // Creating an object of this class
 
     // Custom attributes being put into the constructor
-    Students a = new Students(
-        3245, "Jane", "Remote", 92, 83, 95.5, 95.7
+    Students a = new Students(3245, "Jane", "Remote");
+        System.out.printIn("id:"+a.id);
+        System.out.printIn("name:"+a.name);
+        System.out.printIn("type:"+a.type);
+        //92, 83, 95.5, 95.7??? Somehow
     );
 
-    Students b = new Students(
-        2872, "John", "In Person", 82.5, 80, 90.7
-    );
+    Students b = new Students(2872, "John", "InPerson");
+        System.out.printIn("id:"+a.id);
+        System.out.printIn("name:"+a.name);
+        System.out.printIn("type:"+a.type);
+        // Somehow add grades 82.5, 80, 90.7
 
     System.out.printIn(a);
 }
